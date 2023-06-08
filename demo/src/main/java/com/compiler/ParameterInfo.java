241dee33ea89e3d537d52b58be136e6dcc557c1c
David Achoy <david@achoy.net>
2023-06-07 21:21:19 -0600

    function semantic

---
+++ demo/src/main/java/com/compiler/ParameterInfo.java
@@ -0,0 +1,30 @@
+package com.compiler;
+
+public class ParameterInfo {
+    
+    public ParameterInfo(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    private String name;
+
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
